7cb1f561636e5ae03b6e385964acd0da525445a6
SATYA-GARLAPATI09 <satyagarlapati8@gmail.com>
2026-08-20 21:12:32 +0530

    swap without temp var

---
+++ unit 1/swap.py
@@ -0,0 +1,12 @@
+# Write a program to swap two numbers without using a temporary variable.  
+
+a=30
+b=40
+
+print ("Before swapping the var : a=",a," b=",b)
+#  main logic to swap two numbers without using a temporary variable 
+a= a+b  
+b= a-b
+a=a-b
+print ("after swapping the var : a=",a," b=",b)
+
